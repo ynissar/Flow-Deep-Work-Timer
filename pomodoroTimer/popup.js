10ae6b196617f8139490b9def58ece0ec106a4c5
Waldohatesyou <yusufnissar8@gmail.com>
2022-01-17 19:28:13 -0500

    Reformatted files

--- pomodoroTimer/popup.js
+++ pomodoroTimer/popup.js
@@ -1,16 +1,16 @@
 function setAlarm(event) {
-    let minutes = parseFloat(event.target.value);
-    chrome.browserAction.setBadgeText({text: "ON"});
-    chrome.alarms.create({delayInMinutes: minutes});
-    chrome.storage.sync.set({minutes: minutes});
-    window.close();
+  let minutes = parseFloat(event.target.value);
+  chrome.browserAction.setBadgeText({ text: "ON" });
+  chrome.alarms.create({ delayInMinutes: minutes });
+  chrome.storage.sync.set({ minutes: minutes });
+  window.close();
 }
 
 function clearAlarm() {
-    chrome.browserAction.setBadgeText({text: ''});
-    chrome.alarms.clearAll();
-    window.close();
+  chrome.browserAction.setBadgeText({ text: "" });
+  chrome.alarms.clearAll();
+  window.close();
 }
 
-document.getElementById('start').addEventListener('click', setAlarm);
-document.getElementById('cancel').addEventListener('click', clearAlarm);
+document.getElementById("start").addEventListener("click", setAlarm);
+document.getElementById("cancel").addEventListener("click", clearAlarm);
