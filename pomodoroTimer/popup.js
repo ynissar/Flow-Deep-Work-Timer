7fc73f5128c008444262ef7e4014487fbb5e22f8
Waldohatesyou <yusufnissar8@gmail.com>
2022-02-06 10:36:40 -0500

    Added stylesheet and redid popup.html

--- pomodoroTimer/popup.js
+++ pomodoroTimer/popup.js
@@ -1,143 +1,312 @@
-const timer = document.getElementById("timer");
-let sec = null;
+let alarmTime = new Date();
+let displayCountdown; // global variable for timed display of the count down
+let displayedTime = false;
 
-// Updates sec to synced stored time
-// Must use chrome.storage.sync.get instead of getSeconds as it skips the code block if it doesn't work
-chrome.storage.sync.get(["focusedMinutes"], (result) => {
-  let { seconds } = result;
-  sec = seconds; // CHANGE THIS VAL TO CHANGE TEH SECONDS STORED (BECAUSE WE HAVE A VAL STORED ON CHROME ALL TEH TIME)
-  setSeconds(sec);
-  timer.innerHTML = displayTime(sec);
-});
-
-// If there isn't a time in seconds stored
-if (sec == null) {
-  sec = 25 * 60;
-  timer.innerHTML = displayTime(sec);
-}
+let { curTime, curDate, curHours, curMinutes, curSeconds } =
+  getCurrentTimeData();
+let alarmLengthInMin = 25;
+let alarmLengthInSec = alarmLengthInMin * 60;
 
 document.getElementById("start").addEventListener("click", () => {
-  startTimer(1);
+  if (document.getElementById("start") != null) {
+    startTimer();
+  }
 });
 
-// Displays the time in minutes and seconds
-function displayTime(seconds) {
-  let min = Math.floor(seconds / 60);
-  let sec = seconds % 60;
+document.getElementById("reset").addEventListener("click", restartTimer);
 
-  return `${min}:${sec < 10 ? "0" : ""}${sec}`;
+if (displayedTime == false) {
+  displayTime(alarmLengthInMin);
 }
 
-// starts the timer
-function startTimer(minutes) {}
+chrome.storage.sync.get(["alarmTimeElapsed"], (result) => {
+  let alarmTimeElapsed = result.alarmTimeElapsed;
+  displayTime(Math.floor(alarmTimeElapsed / 1000));
+  displayedTime = true;
+});
 
-// resets the timer
-function resetTimer() {
-  chrome.alarms.clear({ name: "focusTimer" }); // Clears the previous timer of the same name
-}
+continueTimer();
 
-// pauses the timer
-function pauseTimer() {}
+function continueTimer() {
+  chrome.storage.sync.get(["isTimerRunning"], (running) => {
+    let isTimerRunning = running.isTimerRunning;
+    if (isTimerRunning) {
+      chrome.storage.sync.get(["alarmEnd"], (alarm) => {
+        let alarmTime = alarm.alarmEnd;
+        let timeData = getCurrentTimeData();
+        let { curTime } = timeData;
 
-// function allows you to set the time you want to focus for
-function changeTimerLength() {}
+        let difference = Math.abs(alarmTime - curTime);
 
-// plays a sound at the end of the timer
-function playSound() {}
+        displayCountdown = setInterval(() => {
+          displayTime(difference / 1000); // divide by 1000 to make it into seconds
+          if (difference == 0) {
+            clearInterval(countDown);
+          }
+          difference = difference - 1000;
+          chrome.storage.sync.set({ alarmTimeElapsed: difference });
+          console.log(difference);
+        }, 1000);
 
-// opens a page, advises you to meditate for 5 minutes
-function restTimer() {}
+        let startButton = document.getElementById("start");
+        startButton.remove();
 
-// Gets the seconds stored on the timer
-function getSeconds() {
-  let secondStored;
-  chrome.storage.sync.get(["seconds"], (result) => {
-    secondStored = result.seconds;
+        let pauseButton = document.createElement("button");
+        pauseButton.innerHTML = "Pause";
+        pauseButton.id = "pause";
+        pauseButton.classList.add("button");
+
+        let btnGroup = document.getElementById("btn-group");
+        btnGroup.insertBefore(pauseButton, btnGroup.childNodes[0]);
+
+        // creates an event listener for pause
+        document.getElementById("pause").addEventListener("click", () => {
+          if (document.getElementById("pause") != null) {
+            pauseTimer();
+          }
+        });
+      });
+    } else {
+      let startButton = document.getElementById("start");
+      startButton.remove();
+
+      let unpauseButton = document.createElement("button");
+      unpauseButton.innerHTML = "Start";
+      unpauseButton.id = "unpause";
+      unpauseButton.classList.add("button");
+
+      let btnGroup = document.getElementById("btn-group");
+      btnGroup.insertBefore(unpauseButton, btnGroup.childNodes[0]);
+
+      // creates an event listener for pause
+      document.getElementById("unpause").addEventListener("click", () => {
+        if (document.getElementById("unpause") != null) {
+          unpauseTimer();
+        }
+      });
+    }
   });
-  return secondStored;
 }
 
-function setSeconds(newSeconds) {
-  chrome.storage.sync.set({ seconds: newSeconds });
+function startTimer() {
+  let timeData = getCurrentTimeData();
+  let { curTime, curDate, curHours, curMinutes, curSeconds } = timeData;
+
+  calculateAlarmTime(
+    curDate,
+    curHours,
+    curMinutes,
+    curSeconds,
+    alarmLengthInSec,
+    alarmTime
+  );
+
+  let difference = Math.abs(alarmTime - curTime);
+  //   console.log(difference);
+  //   difference = Math.abs(Date.now() + difference - curTime);
+  //   console.log(difference);
+
+  chrome.alarms.create("timer", { when: Date.now() + difference }); // alarm to track when the timer is finished
+
+  chrome.storage.sync.set({ isTimerRunning: true });
+  chrome.storage.sync.set({ alarmEnd: Date.now() + difference });
+
+  displayCountdown = setInterval(() => {
+    displayTime(difference / 1000); // divide by 1000 to make it into seconds
+    if (difference == 0) {
+      clearInterval(countDown);
+    }
+    difference = difference - 1000;
+    chrome.storage.sync.set({ alarmTimeElapsed: difference });
+    console.log(difference);
+  }, 1000);
+
+  //   document.getElementById("start").innerHTML = "pause"; // changed the button to say pause
+  //   document.getElementById("start").id = "pause"; // changes the id of the button to pause
+
+  let startButton = document.getElementById("start");
+  startButton.remove();
+
+  let pauseButton = document.createElement("button");
+  pauseButton.innerHTML = "Pause";
+  pauseButton.id = "pause";
+  pauseButton.classList.add("button");
+
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(pauseButton, btnGroup.childNodes[0]);
+
+  // creates an event listener for pause
+  document.getElementById("pause").addEventListener("click", () => {
+    if (document.getElementById("pause") != null) {
+      pauseTimer();
+    }
+  });
 }
 
-// function setAlarm(event) {
-//   let minutes = parseFloat(event.target.value);
-//   chrome.browserAction.setBadgeText({ text: "ON" });
-//   chrome.alarms.create({ delayInMinutes: minutes });
-//   chrome.storage.sync.set({ minutes: minutes });
-//   window.close();
-// }
+function pauseTimer() {
+  clearInterval(displayCountdown);
 
-// function clearAlarm() {
-//   chrome.browserAction.setBadgeText({ text: "" });
-//   chrome.alarms.clearAll();
-//   window.close();
-// }
+  chrome.storage.sync.set({ isTimerRunning: false });
 
-// const timer = document.getElementById("timer");
-// let seconds = null;
+  chrome.alarms.clear("timer");
 
-// chrome.storage.sync.get(["sec"], (result) => {
-//   let { sec } = result;
-//   seconds = sec - 1;
-//   chrome.storage.sync.set({ sec: seconds });
-//   timer.innerHTML = `${seconds}`;
-// });
+  //   chrome.alarms.get("timer", (alarm) => {
+  //     console.log(alarm);
+  //   });
 
-// if (seconds == null) {
-//   chrome.storage.sync.set({ sec: 75 });
-//   timer.innerHTML = `${seconds}`;
-// }
+  let pauseButton = document.getElementById("pause");
+  pauseButton.remove();
 
-// function thingy() {
-//   chrome.storage.sync.get(["sec"], (result) => {
-//     let { sec } = result;
-//     seconds = sec - 1;
-//     chrome.storage.sync.set({ sec: seconds });
-//   });
-// }
+  let unpauseButton = document.createElement("button");
+  unpauseButton.innerHTML = "Unpause";
+  unpauseButton.id = "unpause";
+  unpauseButton.classList.add("button");
 
-// function setAlarm() {
-//   chrome.storage.sync.get(["sec"], (result) => {
-//     let { sec } = result;
-//     seconds = sec - 1;
-//     chrome.storage.sync.set({ sec: seconds });
-//   });
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(unpauseButton, btnGroup.childNodes[0]);
 
-//   timer.innerHTML = `${seconds}`;
-// }
+  document.getElementById("unpause").addEventListener("click", () => {
+    if (document.getElementById("unpause") != null) {
+      unpauseTimer();
+    }
+  });
+}
 
-// document.getElementById("start").addEventListener("click", setAlarm);
-// document.getElementById("cancel").addEventListener("click", clearAlarm);
+function unpauseTimer() {
+  chrome.storage.sync.set({ isTimerRunning: true });
 
-// const timer = document.getElementById("timer");
-// let timeSec = 75;
+  chrome.storage.sync.get(["alarmTimeElapsed"], (result) => {
+    difference = result.alarmTimeElapsed;
 
-// displayTime(timeSec);
+    chrome.alarms.create("timer", { when: Date.now() + difference }); // alarm to track when the timer is finished
 
-// const countDown = setInterval(() => {
-//   timeSec--;
-//   displayTime(timeSec);
-//   if (timeSec <= 0) {
-//     clearInterval(countDown);
-//   }
-// }, 1000);
+    //console.log(`Difference at start of unpause: ${difference}`);
+    displayCountdown = setInterval(() => {
+      displayTime(difference / 1000); // divide by 1000 to make it into seconds
+      if (difference == 0) {
+        clearInterval(countDown);
+      }
+      difference = difference - 1000;
+      chrome.storage.sync.set({ alarmTimeElapsed: difference });
+      console.log(difference);
+    }, 1000);
+  });
 
-// function displayTime(seconds) {
-//   let min = Math.floor(seconds / 60);
-//   let sec = seconds % 60;
+  let unpauseButton = document.getElementById("unpause");
+  unpauseButton.remove();
 
-//   timer.innerHTML = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
-// }
+  let pauseButton = document.createElement("button");
+  pauseButton.innerHTML = "Pause";
+  pauseButton.id = "pause";
+  pauseButton.classList.add("button");
 
-// const for getting the timer by id
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(pauseButton, btnGroup.childNodes[0]);
 
-// Code for first getting the amount in seconds (then if it fails), if statement to set seconds in sync
+  document.getElementById("pause").addEventListener("click", () => {
+    if (document.getElementById("pause") != null) {
+      pauseTimer();
+    }
+  });
+}
 
-// alarm = chrome.alarms.get("focusedAlarm");
-//   alarm.then((alarm) => {
-//     if (alarm) {
-//       console.log(alarm.name);
-//     }
+function restartTimer() {
+  clearInterval(displayCountdown);
+
+  chrome.alarms.clear("timer");
+  chrome.storage.sync.set({ isTimerRunning: false });
+  chrome.storage.sync.set({ alarmTimeElapsed: alarmLengthInSec * 1000 });
+
+  displayTime(alarmLengthInSec);
+
+  let button;
+
+  if (document.getElementById("pause") != null) {
+    button = document.getElementById("pause");
+  } else {
+    button = document.getElementById("unpause");
+  }
+
+  button.remove();
+
+  let startButton = document.createElement("button");
+  startButton.innerHTML = "Start";
+  startButton.id = "start";
+  startButton.classList.add("button");
+
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(startButton, btnGroup.childNodes[0]);
+
+  document.getElementById("start").addEventListener("click", startTimer);
+}
+
+// Updates timer to reflect
+function displayTime(alarmTimeLeftInSec) {
+  let min = Math.floor(alarmTimeLeftInSec / 60);
+  let sec = Math.floor(alarmTimeLeftInSec % 60);
+  const timer = document.getElementById("timer");
+  timer.innerHTML = `${min}:${sec < 10 ? "0" : ""}${sec}`;
+}
+
+// Returns current time object with all important properties
+function getCurrentTimeData() {
+  let curTime = new Date();
+  let curData = {
+    curTime: curTime,
+    curDate: curTime.getDate(),
+    curHours: curTime.getHours(),
+    curMinutes: curTime.getMinutes(),
+    curSeconds: curTime.getSeconds(),
+  };
+
+  return curData;
+}
+
+// Updates the given date object (alarmTime) with the day, hour, minute and second when the alarm should trigger
+function calculateAlarmTime(
+  curDate,
+  curHours,
+  curMinutes,
+  curSeconds,
+  alarmLengthInSec,
+  alarmTime
+) {
+  let newSeconds = curSeconds + alarmLengthInSec;
+  let addedMinutes = 0; // Minutes added from seconds (>=60)
+  let addedHours = 0; // Hours added from minutes (>=60)
+  let addedDays = 0; // Day added from hours over (>=24)
+
+  // If seconds after adding the alarmLength >=60, shift it over to minutes
+  if (newSeconds >= 60) {
+    addedMinutes = Math.floor(newSeconds / 60);
+    newSeconds = newSeconds % 60;
+  }
+  alarmTime.setSeconds(newSeconds); // Updates seconds in alarmTime
+
+  let newMinutes = curMinutes + addedMinutes;
+
+  // If minutes after adding the minutes from seconds >= 60, shift it over to hours
+  if (newMinutes >= 60) {
+    addedHours = Math.floor(newMinutes / 60);
+    newMinutes = newMinutes % 60;
+  }
+  alarmTime.setMinutes(newMinutes); // Updates minutes in alarmTime
+
+  let newHours = curHours + addedHours;
+
+  // If hours after adding the hours from minutes >= 60, shift it over to days
+  if (newHours >= 24) {
+    addedDays = Math.floor(newHours / 24);
+    newHours = newHours % 24;
+  }
+  alarmTime.setHours(newHours); // Updates hours in alarmTime
+
+  // Adds an extra day if days from hours >= 24
+  newDate = curDate + addedDays;
+
+  alarmTime.setDate(newDate); // Updates date in alarmTime
+}
+
+// chrome.storage.sync.get(["isTimerRunning"], (result) => {
+//     console.log(result.isTimerRunning);
 //   });
