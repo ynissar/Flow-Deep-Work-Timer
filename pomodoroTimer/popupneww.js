3bfcd134d54c48d94ae5db8872bd163b49b807a8
Waldohatesyou <yusufnissar8@gmail.com>
2022-02-05 21:51:42 -0500

    Added pause, unpause and restart functions

--- pomodoroTimer/popupneww.js
+++ pomodoroTimer/popupneww.js
@@ -1,4 +1,5 @@
 let alarmTime = new Date();
+let displayCountdown; // global variable for timed display of the count down
 
 let { curTime, curDate, curHours, curMinutes, curSeconds } =
   getCurrentTimeData();
@@ -10,6 +11,8 @@ document.getElementById("start").addEventListener("click", () => {
     startTimer();
   }
 });
+
+document.getElementById("reset").addEventListener("click", restartTimer);
 
 displayTime(alarmLengthInSec);
 function continueTimer() {}
@@ -31,20 +34,33 @@ function startTimer() {
 
   chrome.alarms.create("timer", { when: Date.now() + difference }); // alarm to track when the timer is finished
 
-  chrome.storage.sync.set({
-    alarmInfo: { isTimerRunning: true, alarmEnd: Date.now() + difference },
-  });
+  chrome.storage.sync.set({ isTimerRunning: true });
+  chrome.storage.sync.set({ alarmEnd: Date.now() + difference });
 
-  let countDown = setInterval(() => {
+  displayCountdown = setInterval(() => {
     displayTime(difference / 1000); // divide by 1000 to make it into seconds
     if (difference == 0) {
       clearInterval(countDown);
     }
     difference = difference - 1000;
+    chrome.storage.sync.set({ alarmTimeElapsed: difference });
+    console.log(difference);
   }, 1000);
 
-  document.getElementById("start").innerHTML = "pause";
-  document.getElementById("start").id = "pause";
+  //   document.getElementById("start").innerHTML = "pause"; // changed the button to say pause
+  //   document.getElementById("start").id = "pause"; // changes the id of the button to pause
+
+  let startButton = document.getElementById("start");
+  startButton.remove();
+
+  let pauseButton = document.createElement("button");
+  pauseButton.innerHTML = "pause";
+  pauseButton.id = "pause";
+
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(pauseButton, btnGroup.childNodes[0]);
+
+  // creates an event listener for pause
   document.getElementById("pause").addEventListener("click", () => {
     if (document.getElementById("pause") != null) {
       pauseTimer();
@@ -53,10 +69,96 @@ function startTimer() {
 }
 
 function pauseTimer() {
-  console.log(document.getElementById("pause").id);
+  clearInterval(displayCountdown);
+
+  chrome.storage.sync.set({ isTimerRunning: false });
+
+  chrome.alarms.clear("timer");
+
+  //   chrome.alarms.get("timer", (alarm) => {
+  //     console.log(alarm);
+  //   });
+
+  let pauseButton = document.getElementById("pause");
+  pauseButton.remove();
+
+  let unpauseButton = document.createElement("button");
+  unpauseButton.innerHTML = "unpause";
+  unpauseButton.id = "unpause";
+
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(unpauseButton, btnGroup.childNodes[0]);
+
+  document.getElementById("unpause").addEventListener("click", () => {
+    if (document.getElementById("unpause") != null) {
+      unpauseTimer();
+    }
+  });
 }
 
-function restartTimer() {}
+function unpauseTimer() {
+  chrome.storage.sync.set({ isTimerRunning: true });
+
+  chrome.storage.sync.get(["alarmTimeElapsed"], (result) => {
+    difference = result.alarmTimeElapsed;
+
+    chrome.alarms.create("timer", { when: Date.now() + difference }); // alarm to track when the timer is finished
+
+    //console.log(`Difference at start of unpause: ${difference}`);
+    displayCountdown = setInterval(() => {
+      displayTime(difference / 1000); // divide by 1000 to make it into seconds
+      if (difference == 0) {
+        clearInterval(countDown);
+      }
+      difference = difference - 1000;
+      chrome.storage.sync.set({ alarmTimeElapsed: difference });
+      console.log(difference);
+    }, 1000);
+  });
+
+  let unpauseButton = document.getElementById("unpause");
+  unpauseButton.remove();
+
+  let pauseButton = document.createElement("button");
+  pauseButton.innerHTML = "pause";
+  pauseButton.id = "pause";
+
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(pauseButton, btnGroup.childNodes[0]);
+
+  document.getElementById("pause").addEventListener("click", () => {
+    if (document.getElementById("pause") != null) {
+      pauseTimer();
+    }
+  });
+}
+
+function restartTimer() {
+  clearInterval(displayCountdown);
+
+  chrome.alarms.clear("timer");
+
+  displayTime(alarmLengthInSec);
+
+  let button;
+
+  if (document.getElementById("pause") != null) {
+    button = document.getElementById("pause");
+  } else {
+    button = document.getElementById("unpause");
+  }
+
+  button.remove();
+
+  let startButton = document.createElement("button");
+  startButton.innerHTML = "start";
+  startButton.id = "start";
+
+  let btnGroup = document.getElementById("btn-group");
+  btnGroup.insertBefore(startButton, btnGroup.childNodes[0]);
+
+  document.getElementById("start").addEventListener("click", startTimer);
+}
 
 // Updates timer to reflect
 function displayTime(alarmTimeLeftInSec) {
@@ -124,3 +226,7 @@ function calculateAlarmTime(
 
   alarmTime.setDate(newDate); // Updates date in alarmTime
 }
+
+// chrome.storage.sync.get(["isTimerRunning"], (result) => {
+//     console.log(result.isTimerRunning);
+//   });
